db26c52d33ae4652a33e9c80cb9f5e36d8f06c79
aboneda <abdelrahmanaboneda@gmail.com>
2024-03-24 14:36:16 +0100

    launchpad

--- app/(main)/agency/[agenycId]/launchpad/page.tsx
+++ app/(main)/agency/[agenycId]/launchpad/page.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { FcAndroidOs } from "react-icons/fc";
+import { ImAppleinc } from "react-icons/im";
+import { FaApple, FaStripe } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { CheckCircle } from "lucide-react";
 
 const Launchpad = () => {
-  return <div>Launchpad</div>;
+  return (
+    <div className="w-full h-full ">
+      <div className="w-full p-5 rounded-lg bg-muted/10">
+        <h1 className="text-lg font-medium">Lets get Started!</h1>
+        <h3 className="text-muted-foreground/70">
+          follow the steps below to get your account setup
+        </h3>
+
+        <div className="p-3 flex flex-col">
+          <div className="rounded-lg border flex items-center justify-between my-5">
+            <div className="flex w-full items-center justify-between px-4">
+              <div className="flex items-center gap-2 p-5">
+                <FcAndroidOs className="w-12 h-12 bg-gray-600 rounded-full" />
+                <FaApple className="w-12 h-12 bg-gray-600 rounded-full p-1" />
+                Save the website as a shortcut on your mobile device
+              </div>
+
+              <Button size={"lg"}>Start</Button>
+            </div>
+          </div>
+
+          <div className="rounded-lg border flex items-center justify-between my-5">
+            <div className="flex w-full items-center justify-between px-4">
+              <div className="flex items-center gap-2 p-5">
+                <FaStripe className="w-12 h-12 rounded-full text-violet-800" />
+                Connect Your stripe account to accept payments
+              </div>
+
+              <Button size={"lg"}>Start</Button>
+            </div>
+          </div>
+
+          <div className="rounded-lg border flex items-center justify-between my-5">
+            <div className="flex w-full items-center justify-between px-4">
+              <div className="flex items-center gap-2 p-5">
+                <Image
+                  width={55}
+                  height={50}
+                  src={"/assets/logo.png"}
+                  alt="logo"
+                  className="rounded-md object-cover mr-4"
+                />
+                Fill in all your business details
+              </div>
+
+              <CheckCircle className="text-violet-600 w-12 h-12 mr-7" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Launchpad;
--- app/(main)/agency/[agenycId]/subaccounts/page.tsx
+++ app/(main)/agency/[agenycId]/subaccounts/page.tsx
@@ -1,7 +1,73 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import { PlusCircle, Search } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const Subaccounts = () => {
-  return <div>Subaccounts</div>;
+  const subaccounts = [
+    {
+      logo: "/assets/logo.png",
+      address: "1244 street",
+      name: "Bolt",
+    },
+    {
+      logo: "/assets/logo2.jpg",
+      address: "98698 street",
+      name: "Company",
+    },
+  ];
+  return (
+    <div className="p-3 flex flex-col">
+      <div className="w-full flex flex-col ">
+        <div className="flex items-center justify-end">
+          <Button className="max-w-xs flex space-x-2 flex-end">
+            <PlusCircle className="h-4 w-4" />
+            <p>Create Sub Account</p>
+          </Button>
+        </div>
+
+        <div className="relative mt-5 ">
+          <Search className="absolute left-2 top-3 h-4 w-4 text-muted-foreground" />
+          <Input placeholder="Search" className="pl-8 bg-muted-foreground/10" />
+        </div>
+        <div className="p-3 flex flex-col items-center justify-start w-full ">
+          <p className="text-muted-foreground/60  w-full justify-start flex">
+            Sub Accounts
+          </p>
+          <div className="flex flex-col w-full mt-5 space-y-7">
+            {subaccounts.map((sub, index) => (
+              <div
+                key={index}
+                className="flex p-3 bg-muted/30 items-center justify-between w-full rounded-lg"
+              >
+                <div className="w-full flex items-center justify-start ">
+                  <Image
+                    width={80}
+                    height={50}
+                    src={sub.logo}
+                    alt="logo"
+                    className="rounded-md object-cover mr-4"
+                  />
+
+                  <div className="flex flex-col gap-2">
+                    <h3 className="font-medium">{sub.name}</h3>
+                    <h4 className="text-muted-foreground">{sub.address}</h4>
+                  </div>
+                </div>
+
+                <Button size={"lg"} variant={"destructive"}>
+                  Delete
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Subaccounts;
